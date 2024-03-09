b15c5a5baf05f8af4abbb506b0756a7421f59574
sliterok <12751644+sliterok@users.noreply.github.com>
2024-03-09 10:18:06 +0100

    pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import rakkas from 'rakkasjs/vite-plugin'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	base: 'opfs-demo',
+	base: '/opfs-demo/',
 	plugins: [
 		tsconfigPaths(),
 		react(),
