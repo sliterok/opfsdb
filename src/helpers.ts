e560c728fb1e556499e91390589e0336db2ce084
sliterok <12751644+sliterok@users.noreply.github.com>
2024-03-09 10:39:22 +0100

    fix parse error

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,5 +2,6 @@ import { IFetchDb } from './db/types'
 
 export const dbFetch: IFetchDb = async (url, body) => {
 	const response = await fetch(url, { body: JSON.stringify(body), method: 'POST' })
+	if (response.status === 501) return
 	return response.json()
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import rakkas from 'rakkasjs/vite-plugin'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	base: '/opfs-demo/',
 	plugins: [
 		tsconfigPaths(),
 		react(),
